819a7dd762a0ddc25d99705eca517fec38d160b6
Div <divyanshubramhecha25@kgpian.iitkgp.ac.in>
2026-08-21 17:57:39 +0530

    added preload auto to video

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -32,6 +32,7 @@ export default function IntroScreen({ onEnter }: IntroScreenProps) {
           src="/capvideo.mp4"
           autoPlay
           playsInline
+          preload="auto"
           className="absolute top-1/2 left-1/2 w-[100vh] h-[100vw] object-cover z-20 -translate-x-1/2 -translate-y-1/2 -rotate-90"
           ref={(el) => {
             if (el) el.playbackRate = 1.0;
